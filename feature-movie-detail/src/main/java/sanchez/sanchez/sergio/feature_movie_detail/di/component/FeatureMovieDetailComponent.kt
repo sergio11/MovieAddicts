0486defe6f0ee4aa201eac55b40cbb9cc226855b
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2021-02-06 13:07:55 +0100

    Basic Implementation of Movie Detail

--- feature-movie-detail/src/main/java/sanchez/sanchez/sergio/feature_movie_detail/di/component/FeatureMovieDetailComponent.kt
+++ feature-movie-detail/src/main/java/sanchez/sanchez/sergio/feature_movie_detail/di/component/FeatureMovieDetailComponent.kt
@@ -1,21 +1,30 @@
-package sanchez.sanchez.sergio.feature_movie_detail.di
+package sanchez.sanchez.sergio.feature_movie_detail.di.component
 
 import dagger.Component
+import sanchez.sanchez.sergio.feature_movie_detail.di.module.FeatureMovieDetailModule
 import sanchez.sanchez.sergio.feature_movie_detail.ui.FeatureMovieDetailActivity
-
 import sanchez.sanchez.sergio.test.core.di.component.ApplicationComponent
 import sanchez.sanchez.sergio.test.core.di.module.ActivityModule
 import sanchez.sanchez.sergio.test.core.di.module.ViewModelModule
 import sanchez.sanchez.sergio.test.core.di.scope.PerActivity
 
+/**
+ * Feature Movie Detail Component
+ */
 @PerActivity
 @Component(
     modules = [
+        ActivityModule::class,
         ViewModelModule::class,
         FeatureMovieDetailModule::class],
     dependencies = [ApplicationComponent::class]
 )
 interface FeatureMovieDetailComponent {
+
+    /**
+     * Sub-components
+     */
+    fun movieDetailComponent(): MovieDetailComponent
 
     /**
      * Inject into Feature Movie Detail Activity
